88ae25107a4ebe2abc7b000f8eab25c69cc7bac5
Rishabh Verma <2000rishabhverma@gmail.com>
2024-11-24 14:21:48 +0530

    second commit

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,22 @@
-const express = require('express')
-const app = express()
-const notes = require('./data/notes')
+const express = require('express');
+const app = express();
+const notes = require('./data/notes');
 
-app.get('/', (req, res)=>{
-	res.send("Hello");
-})
+app.get('/', (req, res) => {
+    res.send("Hello");
+});
 
-app.get('/api/notes', (req, res)=> {
-	res.json(notes)
-})
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
 
-app.get('/api/notes/:id', (req, res)=> {
-	const note = notes.find((n) => n._id == req.params.id)
-	
-	console.log(req.params)
-	res.sen(dnote);
-})
+app.get('/api/notes/:id', (req, res) => {
+    const note = notes.find(n => n._id == req.params.id);
+    if (!note) {
+        return res.status(404).json({ message: "Note not found" });
+    }
+    res.json(note);
+});
 
-app.listen(process.env.PORT, console.log(`Server started on port ${process.env.PORT}`))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server started on http://localhost:${PORT}/`));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import './bootstrap.min.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import LandingPage from "./screens/LandingPage";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+        <main style={{minHeight: "93vh"}}>
+            <LandingPage />
+        </main>
+      <Footer />
+    </>
+  )
 }
 
 export default App;
